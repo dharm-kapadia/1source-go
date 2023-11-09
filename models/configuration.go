f8579119e60fca4737c22eab403ea49132d29c61
Dharm Kapadia <dharm.kapadia@equilend.com>
2023-11-09 18:34:22 +0000

    Add Configuration TOML model

---
+++ models/configuration.go
@@ -0,0 +1,29 @@
+package models
+
+type (
+	AppConfig struct {
+		General        general
+		Endpoints      endpoints
+		Authentication authentication
+	}
+
+	general struct {
+		Auth_URL string
+	}
+
+	endpoints struct {
+		Events     string
+		Parties    string
+		Agreements string
+		Contracts  string
+	}
+
+	authentication struct {
+		Auth_Type     string
+		Grant_Type    string
+		Client_Id     string
+		Username      string
+		Password      string
+		Client_Secret string
+	}
+)
